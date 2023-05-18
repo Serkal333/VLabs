cd21e5e003224740c7092881dc8cc6cdd88ff928
Serkal333 <sergei.kala1chev@gmail.com>
2023-05-18 22:56:19 +0300

    SMOI lab_1 page1-complete

--- SMOI/lab_1/lab_1.py
+++ SMOI/lab_1/lab_1.py
@@ -1,14 +1,16 @@
 import matplotlib.pyplot as plt
+from scipy.stats import norm
 import numpy as np
 import copy
 
-#   1.1     Начальные значения
+#   Начальные значения
 variant = 17
 np.random.seed(variant)
 item_size = 100
-otkl = variant/100
-
 x = list (range(1,101))
+
+#   Лист 1. Нормальное распределение
+otkl = variant/100
 normal = np.random.normal(variant, otkl, item_size)
 
 
@@ -31,20 +33,31 @@ for i in range (0, item_size):
 #otn_chast = np.arange(float(pocket_count+1)*0)
 otn_chast = copy.copy(pocket_value)/item_size
 
+norm_rasp = np.arange(float(pocket_count+1))*0
+for i in range (0, norm_rasp.size):
+    norm_rasp[i] = norm.pdf(pocket_list[i], variant, otkl)
+
+nr = copy.copy(norm_rasp)/sum(norm_rasp)
+
 f_y = copy.copy(otn_chast)
 for i in range (1, f_y.size):
     f_y[i]=f_y[i-1]+f_y[i]
 
+#   Лист 2. Равномерное распределение
+
+ravn = np.random.uniform(variant, variant*2, item_size)
+
+
+#   Отрисовка
 
 figure, axis = plt.subplots(2, 4)
 axis[0, 0].plot(x, normal)
 axis[0, 0].set_title('Title 1')
 axis[0, 1].hist(normal, edgecolor = 'black', bins=12)
-axis[0, 2].hist(normal, edgecolor = 'black', bins=12)
-
+axis[0, 2].hist(normal,  weights=np.ones_like (normal) / len (normal), edgecolor = 'black', bins=12)
+axis[0, 2].plot(pocket_list, nr)
 axis[0, 3].plot(pocket_list, f_y)
 
 plt.tight_layout()
 plt.show()
-print(f_y)
-#print(sum(pocket_value))
+print(nr)
